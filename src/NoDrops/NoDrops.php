efc2ee1d169f1da719cfba21418f94e5518cb628
roci33 <44725071+roci33@users.noreply.github.com>
2019-07-11 15:42:05 +0200

    Update NoDrops.php

--- src/NoDrops/NoDrops.php
+++ src/NoDrops/NoDrops.php
@@ -10,6 +10,7 @@ class NoDrops extends PluginBase implements Listener
 
     public function onEnable()
     {
+
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->getLogger()->info("Abilitato");
       @mkdir($this->getDataFolder());
@@ -23,7 +24,10 @@ class NoDrops extends PluginBase implements Listener
     }
 
 public function onDeath(PlayerDeathEvent $event){
-    if(in_array($event->getPlayer()->getLevel()->getName(), [$wconfig->get("Mondi dove è possibile usare il commando")])){
+   $wconfig = new Config($this->getDataFolder()."config.yml", Config::YAML);
+
+
+ if(in_array($event->getPlayer()->getLevel()->getName(), [$wconfig->get("Mondi dove è possibile usare il commando")])){
         $event->setDrops([]);
     }
   }
